323f63ef7156c24ef311b02921d5b8a60d64001c
Rob H <rob.houghton@cdl.co.uk>
2019-08-12 16:48:18 +0100

    Resolving conflicts

--- aws/provider.go
+++ aws/provider.go
@@ -250,6 +250,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_route_tables":                              dataSourceAwsRouteTables(),
 			"aws_route53_delegation_set":                    dataSourceAwsDelegationSet(),
 			"aws_route53_zone":                              dataSourceAwsRoute53Zone(),
+			"aws_route53_resolver_endpoint":                 dataSourceAwsRoute53ResolverEndpoint(),
 			"aws_s3_bucket":                                 dataSourceAwsS3Bucket(),
 			"aws_s3_bucket_object":                          dataSourceAwsS3BucketObject(),
 			"aws_s3_bucket_objects":                         dataSourceAwsS3BucketObjects(),
